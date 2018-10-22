f03799198f5703955ca6ede5f197c1344bdac0c4
Safa Morjane <safa.morjane@gmail.com>
2018-10-22 15:46:16 +0100

    Audio and highscore Added

---
+++ Hexagon/Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    
+    public Text seconds;
+    public float StartTime;
+    public static float timer = 0f;
+
+    private void Start()
+    {
+        StartTime = Time.time;
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+         timer = Time.time-StartTime;
+
+        seconds.text = timer.ToString("f2");
+    }
+}
